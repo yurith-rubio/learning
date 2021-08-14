23e191b13e846ee9b98d6fe459d91fb53c12a304
Yurith Rubio <iamyurith@gmail.com>
2021-08-14 19:29:42 +0200

    Remove unused code and update text

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
-/*window.onscroll = function() {slideRight()};
-
-function slideRight() {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-      document.getElementById("leftIntro").className = "sliding";
-  } else {
-      document.getElementById("leftIntro").className = "";
-    }
-
-}*/
-
-
 
 window.onscroll = function () { expand() };
 
 function expand() {
-    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-      document.getElementById("leftIntro").style.width = "100%";
-      document.getElementById("textContainer").style.width = "0%";
-      
-    }
-    else {
-      document.getElementById("leftIntro").style.width = "50%";
-      document.getElementById("textContainer").style.width = "50%";
-      
-    }
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    document.getElementById("leftIntro").style.width = "100%";
+    document.getElementById("textContainer").style.width = "0%";
+
+  }
+  else {
+    document.getElementById("leftIntro").style.width = "50%";
+    document.getElementById("textContainer").style.width = "50%";
+
+  }
 }
 
 //document.getElementById("textIntro").style.width = "0%";
@@ -33,19 +21,19 @@ function expand() {
 
 window.addEventListener("scroll", function () {
   const distance = window.scrollY;
-    
+
   document.querySelector(".cover").style.transform = `translateY(${distance *
     -.2}px)`;
 
   document.querySelector(".image").style.transform = `translateY(${distance * -.1}px)`;
-  
+
   setTimeout(() => {
     document.querySelector(".section").style.transform = `translateY(${distance *
       -.4}px)`;;
-    
+
   });
-    
-  });
-  
+
+});
+
 
 //  document.querySelector(".image").style.transform = `translateY(${distance * -.01}px)`;
